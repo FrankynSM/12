9f7bfe518ad423a6d21f87ca08f35ddc6d6d23e7
FrankynSM <101581082+FrankynSM@users.noreply.github.com>
2022-06-06 20:26:32 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+// Object Literal enhancement
+
+const banda = 'Metallica'
+const genero = 'Heavy Metal'
+const canciones = ['Master Of Puppets', 'Seek & Destroy', 'Enter Sandman']
+
+// Forma anterior
+// const metallica = {
+//  banda: banda,
+//  genero: genero,
+//  canciones: canciones,
+// }
+
+// Forma nueva
+const metallica = { banda, genero, canciones }
+
+console.log(metallica)
